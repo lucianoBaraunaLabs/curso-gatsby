8475fdd861e4889f660596c2e3f20812a7bbc367
Luciano Baraúna <baraunaluciano@gmail.com>
2019-08-30 15:14:06 -0300

    limpando o diretório

--- my-blog/gatsby-config.js
+++ my-blog/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Meu blog`,
+    description: `Esse blog é feito de acordo com o curso.`,
+    author: `Meu Blog - Luciano Baraúna.`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
