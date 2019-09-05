745f948d9c47ffc0fb2561c0ddfb5b92c9c10ec1
Luciano Baraúna <baraunaluciano@gmail.com>
2019-09-05 11:09:24 -0300

    finalizada aula 26

--- my-blog/src/components/Profile/index.js
+++ my-blog/src/components/Profile/index.js
@@ -1,6 +1,12 @@
 import React from "react";
-import { useStaticQuery, graphql } from "gatsby";
+
+import {
+  useStaticQuery,
+  graphql
+} from "gatsby";
+
 import Avatar from '../Avatar'
+import * as S from './styled';
 
 const Profile = () => {
   const {
@@ -19,12 +25,16 @@ const Profile = () => {
     }
   `)
   return (
-    <div className="Profile-wrapper">
-      <Avatar />
-      <h1>{title}</h1>
-      <h2>{position}</h2>
-      <p>{description}</p>
-    </div>
+    <S.ProfileWrapper>
+      <S.ProfileLink>
+        <Avatar />
+        <S.ProfileAuthor>
+          {title}
+          <S.ProfilePosition>{position}</S.ProfilePosition>
+        </S.ProfileAuthor>
+        <S.ProfileDescription>{description}</S.ProfileDescription>
+      </S.ProfileLink>
+    </S.ProfileWrapper>
   )
 }
 
