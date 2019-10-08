e9736a5220bd9d47218244912fc824f2195a64fe
Luciano Baraúna <baraunaluciano@gmail.com>
2019-10-08 19:28:01 -0300

    Finalizada aula 63

--- my-blog/src/components/Search/index.js
+++ my-blog/src/components/Search/index.js
@@ -2,6 +2,7 @@ import React from "react"
 
 import algoliasearch from "algoliasearch/lite"
 import { InstantSearch, SearchBox, Hits, Stats } from "react-instantsearch-dom"
+import Hit from "./Hit"
 
 import * as S from "./styled"
 
@@ -24,7 +25,7 @@ const Search = () => (
           },
         }}
       />
-      <Hits />
+      <Hits hitComponent={Hit} />
     </InstantSearch>
   </S.SearchWrapper>
 )
