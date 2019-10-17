5fcc0537c1e102ffea54d49808d015e2ac00e1ad
Luciano Baraúna <baraunaluciano@gmail.com>
2019-10-17 12:06:02 -0300

    finalizando aula 76

--- my-blog/gatsby-config.js
+++ my-blog/gatsby-config.js
@@ -85,6 +85,7 @@ module.exports = {
         display: `minimal-ui`,
       },
     },
+    `gatsby-plugin-sitemap`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
